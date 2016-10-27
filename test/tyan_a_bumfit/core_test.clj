ebdcc66cd21d31f3502a6f5c611414a45cba4e94
Matthew Chadwick <post@celeriac.net>
2016-10-27 11:11:45 +0100

    Yan'st commit

---
+++ test/tyan_a_bumfit/core_test.clj
@@ -0,0 +1,7 @@
+(ns tyan-a-bumfit.core-test
+  (:require [clojure.test :refer :all]
+            [tyan-a-bumfit.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
